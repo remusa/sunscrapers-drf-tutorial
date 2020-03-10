08c3e3dfad8df9ee5b9764f60d5da2be384a8f3b
R Sánchez <rene.as.himself@gmail.com>
2020-03-10 17:46:27 -0600

    feat:CRUD operations

--- rental/views.py
+++ rental/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from . import models, serializers
+
+
+class FriendViewset(viewsets.ModelViewSet):
+    queryset = models.Friend.objects.all()
+    serializer_class = serializers.FriendSerializer
+
+
+class BelongingViewset(viewsets.ModelViewSet):
+    queryset = models.Belonging.objects.all()
+    serializer_class = serializers.BelongingSerializer
+
+
+class BorrowedViewset(viewsets.ModelViewSet):
+    queryset = models.Borrowed.objects.all()
+    serializer_class = serializers.BorrowedSerializer
